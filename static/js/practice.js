0aae5897a649d18a2eaef96238c03426c1a8eb1d
Kazumasa Omura <omura@nlp.ist.i.kyoto-u.ac.jp>
2025-10-04 22:56:02 +0900

    update

--- static/js/practice.js
+++ static/js/practice.js
@@ -1,21 +1,21 @@
 const template = (manipulation, value, reset = false) => {
     let key;
     let values;
-    if (manipulation === 'transform') {
-        key = 'transition';
+    if (manipulation === "transform") {
+        key = "transition";
         values = [`transform ${value}s`, `-webkit-transform ${value}s`];
-    } else if (manipulation === 'transit') {
-        key = 'transition';
+    } else if (manipulation === "transit") {
+        key = "transition";
         values = [`background ${value}s`, `background ${value}s`];
-    } else if (manipulation === 'rotate') {
-        key = 'transform';
+    } else if (manipulation === "rotate") {
+        key = "transform";
         values = [`rotateY(${value}deg)`, `rotateY(${value}deg)`];
     } else {
-        throw 'invalid'
+        throw "invalid"
     }
 
     if (reset) {
-        values = ['', '']
+        values = ["", ""]
     }
 
     return {
@@ -25,37 +25,37 @@ const template = (manipulation, value, reset = false) => {
 }
 
 const flip = (page, index, isLast = false, time = 1.0) => {
-    page.removeClass('current');
-    page.css(template('transform', time));
-    page.css(template('rotate', 0, true));
+    page.removeClass("current");
+    page.css(template("transform", time));
+    page.css(template("rotate", 0, true));
     // エフェクトの切り替え
-    page.children('div.effect.one').removeClass('one').addClass('two');
-    page.children('div.effect.two').css(template('transit', time / 2));
+    page.children("div.effect.one").removeClass("one").addClass("two");
+    page.children("div.effect.two").css(template("transit", time / 2));
 
     // 右のページ
     if (index % 2 === 0) {
         // クリックしたページとその裏側のページが裏返る
-        page.removeClass('flipped');
-        page.prev().removeClass('flipped');
-        page.next().removeClass('current');
-        page.prev().addClass('current');
-        page.prev().prev().addClass('current');
+        page.removeClass("flipped");
+        page.prev().removeClass("flipped");
+        page.next().removeClass("current");
+        page.prev().addClass("current");
+        page.prev().prev().addClass("current");
 
-        page.prev().css(template('transform', time));
-        page.prev().children('div.effect.two').removeClass('two').addClass('one');
-        page.prev().children('div.effect.one').css(template('transit', time / 2));
+        page.prev().css(template("transform", time));
+        page.prev().children("div.effect.two").removeClass("two").addClass("one");
+        page.prev().children("div.effect.one").css(template("transit", time / 2));
     }
     // 左のページ
     else {
-        page.addClass('flipped');
-        page.next().addClass('flipped');
-        page.prev().removeClass('current');
-        page.next().addClass('current');
-        page.next().next().addClass('current');
+        page.addClass("flipped");
+        page.next().addClass("flipped");
+        page.prev().removeClass("current");
+        page.next().addClass("current");
+        page.next().next().addClass("current");
 
-        page.next().css(template('transform', time));
-        page.next().children('div.effect.two').removeClass('two').addClass('one');
-        page.next().children('div.effect.one').css(template('transit', time / 2));
+        page.next().css(template("transform", time));
+        page.next().children("div.effect.two").removeClass("two").addClass("one");
+        page.next().children("div.effect.one").css(template("transit", time / 2));
     }
 
     n_process += 1;
@@ -63,79 +63,79 @@ const flip = (page, index, isLast = false, time = 1.0) => {
         n_process -= 1;
         if (isLast) {
             if (index % 2 === 0) {
-                page.prev().css(template('transform', 1.5));
-                page.prev().prev().css(template('transform', 1.5));
+                page.prev().css(template("transform", 1.5));
+                page.prev().prev().css(template("transform", 1.5));
             } else {
-                page.next().css(template('transform', 1.5));
-                page.next().next().css(template('transform', 1.5));
+                page.next().css(template("transform", 1.5));
+                page.next().next().css(template("transform", 1.5));
             }
         }
     }, time * 1000);
 }
 
 // initialize
-const root = $('#pages');  // tips. $('.pages *')とすれば配下全ての子要素を取得できる
+const root = $("#pages");  // tips. $(".pages *")とすれば配下全ての子要素を取得できる
 const pages = root.children();
-const colors = ['FF8888', 'FFFF88', 'AADDAA', '88FFFF', '8888FF'];
+const colors = ["FF8888", "FFFF88", "AADDAA", "88FFFF", "8888FF"];
 let count = 0;
 let n_process = 0;
 
 pages.each(function (i, _) {
     let page = $(this);
     if (i === 0) {
-        page.addClass('current');
+        page.addClass("current");
     }
 
     if (i % 2 === 0) {
         page.css({"z-index": (pages.length - i)});
 
-        if (page.children().hasClass('bookmark')) {
-            let bookmark = page.children('div.bookmark');
+        if (page.children().hasClass("bookmark")) {
+            let bookmark = page.children("div.bookmark");
             bookmark.css({
-                'top': `min(${2.5 + 4 * count}vw, ${2.5 + 4 * count}vh)`,
-                'border-right': `solid min(2vw, 2vh) #${colors[count % 5]}`
+                "top": `min(${2.5 + 4 * count}vw, ${2.5 + 4 * count}vh)`,
+                "border-right": `solid min(2vw, 2vh) #${colors[count % 5]}`
             });
             count += 1;
         }
     } else {
-        if (page.children().hasClass('bookmark')) {
-            let bookmark = page.children('div.bookmark');
+        if (page.children().hasClass("bookmark")) {
+            let bookmark = page.children("div.bookmark");
             bookmark.css({
-                'top': `min(${2.5 + 4 * (count - 1)}vw, ${2.5 + 4 * (count - 1)}vh)`,
-                'border-left': `solid min(2vw, 2vh) #${colors[(count - 1) % 5]}`
+                "top": `min(${2.5 + 4 * (count - 1)}vw, ${2.5 + 4 * (count - 1)}vh)`,
+                "border-left": `solid min(2vw, 2vh) #${colors[(count - 1) % 5]}`
             });
         }
     }
 });
 
-$(window).on('load', function () {
-    $('.page')
+$(window).on("load", function () {
+    $(".page")
         .click(function () {
-                if (n_process === 0 && $(this).hasClass('current')) {
+                if (n_process === 0 && $(this).hasClass("current")) {
                     let page = $(this);
                     let index = pages.index(page) + 1;
                     flip(page, index);
                 }
             })
         .hover(function () {
-                if ($(this).hasClass('current')){
+                if ($(this).hasClass("current")){
                     let page = $(this);
                     let index = pages.index(page) + 1;
-                    page.css(template('rotate', (index % 2 === 0) ? 10 : -10));
+                    page.css(template("rotate", (index % 2 === 0) ? 10 : -10));
                 }
             },
             function () {
                 let page = $(this);
-                page.css(template('rotate', 0, true));
+                page.css(template("rotate", 0, true));
             });
 
-    $('.bookmark')
+    $(".bookmark")
         .click(function (event) {
             if (n_process === 0) {
                 // bookmarkはpageの子要素なので、イベントを親要素まで伝搬させない
                 event.stopPropagation();
                 const bookmark = $(this);
-                let src = (root.children('div.page').hasClass('flipped')) ? pages.index((root.children('div.page.flipped').slice(-1)[0])) + 2 : 1;
+                let src = (root.children("div.page").hasClass("flipped")) ? pages.index((root.children("div.page.flipped").slice(-1)[0])) + 2 : 1;
                 let tgt = pages.index(bookmark.parent()) + 1;
                 if (tgt % 2 === 0) {
                     src -= 1;
@@ -155,12 +155,12 @@ $(window).on('load', function () {
         })
         .hover(
             function (event) {
-                if (!$(this).parent().hasClass('current')) {
+                if (!$(this).parent().hasClass("current")) {
                     event.stopPropagation();
                 }
             },
             function (event) {}
         );
 
-    $('#book').addClass('bound');
+    $("#book").addClass("bound");
 });
